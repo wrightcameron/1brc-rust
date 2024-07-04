79a54b612c10e5a63e4ad571f3237ac5fb92ebc4
Cameron Wright <ccameronwright@gmail.com>
2024-07-03 18:10:40 -0600

    Moved redundent code in bashscript into for loop.

--- tools/downloadSampleData.sh
+++ tools/downloadSampleData.sh
@@ -5,33 +5,20 @@ BASE_URL="https://raw.githubusercontent.com/gunnarmorling/1brc/main"
 # Sample 44k row data 
 mkdir -p $SCRIPT_DIR/../data/samples
 URL="$BASE_URL/data"
-wget "$URL/weather_stations.csv" -P $SCRIPT_DIR/../data/
+if [ ! -f "$SCRIPT_DIR/../data/weather_stations.csv" ]; then
+    wget --quiet "$URL/weather_stations.csv" -P $SCRIPT_DIR/../data/ || echo "Failed to download: $URL/weather_stations.csv"
+fi
 
-# TODO Change this to a loop that loops over set of strings, and check if the file exists in the data dir first before requesting.
-# Sample data for testing
+files="measurements-1 measurements-10 measurements-10000-unique-keys measurements-2 measurements-20 measurements-3 measurements-boundaries measurements-complex-utf8 measurements-dot measurements-rounding measurements-short measurements-shortest"
 URL="$BASE_URL/src/test/resources/samples"
-wget "$URL/measurements-1.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-1.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-10.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-10.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-10000-unique-keys.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-10000-unique-keys.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-2.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-2.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-20.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-20.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-3.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-3.txt" -P $SCRIPT_DIR/../data/samples
 
-wget "$URL/measurements-boundaries.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-boundaries.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-complex-utf8.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-complex-utf8.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-dot.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-dot.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-rounding.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-rounding.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-short.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-short.txt" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-shortest.out" -P $SCRIPT_DIR/../data/samples
-wget "$URL/measurements-shortest.txt" -P $SCRIPT_DIR/../data/samples
+# Sample data for testing
+for file in $files;
+do
+    if [ ! -f "$SCRIPT_DIR/../data/samples/$file.out" ]; then
+        wget --quiet "$URL/$file.out" -P $SCRIPT_DIR/../data/samples || echo "Failed to download: $URL/$file.out"
+    fi
+    if [ ! -f "$SCRIPT_DIR/../data/samples/$file.txt" ]; then
+        wget --quiet "$URL/$file.txt" -P $SCRIPT_DIR/../data/samples || echo "Failed to download: $URL/$file.txt"
+    fi
+done
